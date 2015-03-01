44a1b399659ce84eea74ff83f4ee639b8c4df25e
Florin Ioan Farcas <ffi82@users.noreply.github.com>
2015-03-01 14:52:07 +0100

    Tiberium Alliances Repair Time Of Death 1.0.0
    Displays how much offense repair time a dead base had at the time of death

--- Tiberium_Alliances_Repair_Time_Of_Death.user.js
+++ Tiberium_Alliances_Repair_Time_Of_Death.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name           Tiberium Alliances Repair Time Of Death
-// @version        1.0.0
-// @namespace      http://openuserjs.org/users/petui
-// @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @author         petui
-// @description    Displays how much offense repair time a dead base had at the time of death
-// @include        http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
-// @grant          none
+// @name	Tiberium Alliances Repair Time Of Death
+// @version	1.0.0
+// @namespace	http://openuserjs.org/users/petui
+// @license	GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @author	petui
+// @description	Displays how much offense repair time a dead base had at the time of death
+// @include	http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @grant	none
 // ==/UserScript==
 'use strict';
 
