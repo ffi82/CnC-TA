7b34819078d8aa166a8955d85d567ff1da837d44
Florin Ioan Farcas <ffi82@users.noreply.github.com>
2016-12-26 20:55:27 +0100

    Tiberium Alliances Real POI Bonus 1.0.1
    Displays actual gain/loss for POIs by taking rank multiplier properly into account
    
    Changes:
    1.0.1 (2016-04-17) - Added updateURL meta required by OUJS
    1.0.0 (2014-10-13) - Initial release
    
    Author: https://openuserjs.org/users/petui

--- Tiberium_Alliances_Real_POI_Bonus.user.js
+++ Tiberium_Alliances_Real_POI_Bonus.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name           Tiberium Alliances Real POI Bonus
-// @version        1.0.0
-// @namespace      http://openuserjs.org/users/petui
+// @version        1.0.1
+// @namespace      https://openuserjs.org/users/petui
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @author         petui
 // @description    Displays actual gain/loss for POIs by taking rank multiplier properly into account
 // @include        http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
-// @grant          none
+// @updateURL      https://openuserjs.org/meta/petui/Tiberium_Alliances_Real_POI_Bonus.meta.js
 // ==/UserScript==
 'use strict';
 
