9054dfde10860e4864044ac6a3353ea02a1acd91
Florin Ioan Farcas <ffi82@users.noreply.github.com>
2025-08-16 15:43:07 +0200

    Update CnCTA-BaseNavBar-MR.user.js
    fix:  first base in the NavBar also shows the MR
    added: show time since last move cooldown ended (right-click on the BaseNaveBar to toggle visibility... show/hide green values)
    
    red = move recovery (countdown)
    green = time since the last move cooldown ended (countup)

--- CnCTA-BaseNavBar-MR.user.js
+++ CnCTA-BaseNavBar-MR.user.js
@@ -1,51 +1,58 @@
 // ==UserScript==
 // @name        CnCTA BaseNavigationBar MR
-// @version     2025.03.24
-// @description Shows base move recovery on the Base Navigation Bar.
+// @version     2025.08.16
+// @description On the Base Navigation Bar it shows the base move recovery (red) or time since the last move cooldown ended (green); right-click navbar to toggle the green values.
 // @author      bloofi (https://github.com/bloofi)
 // @contributor ffi82
+// @updateURL   https://github.com/ffi82/CnC-TA/raw/refs/heads/master/CnCTA-BaseNavBar-MR.meta.js
 // @downloadURL https://github.com/ffi82/CnC-TA/raw/refs/heads/master/CnCTA-BaseNavBar-MR.user.js
-// @updateURL   https://github.com/ffi82/CnC-TA/raw/refs/heads/master/CnCTA-BaseNavBar-MR.user.js
 // @match       https://*.alliances.commandandconquer.com/*/index.aspx*
 // @grant       none
 // ==/UserScript==
-/* global qx, ClientLib */
-"use strict";
+/* global qx, ClientLib, webfrontend */
 (() => {
-    const BaseNavigationBarMR = () => {
-        const labels = {};
-        const updateMoveRecovery = () => {
-            Object.values(ClientLib.Data.MainData.GetInstance().get_Cities().get_AllCities().d).forEach(c => {
-                const time = c.get_MoveCooldownEndStep() - ClientLib.Data.MainData.GetInstance().get_Time().GetServerStep();
-                const s = ClientLib.Data.MainData.GetInstance().get_Time().GetTimespanString(time, 0);
-                labels[`base-${c.get_Id()}`]?.setValue(time > 0 ? s : '');
-            });
-        };
-        const initLabels = (basePanels) => {
-            basePanels.forEach(p => {
-                if (p.getBaseId && p.getBaseId()) {
-                    const label = new qx.ui.basic.Label('').set({ textColor: '#cc0000' });
-                    labels[`base-${p.getBaseId()}`] = label;
-                    p.add(label, { bottom: 2, right: 0 });
-                }
-            });
-            setInterval(updateMoveRecovery, 1000);
-        };
-        const checkForBases = () => {
-            const basePanels = qx.core.Init.getApplication().getBaseNavigationBar().getChildren()[0].getChildren()[0].getChildren();;
-            basePanels.length > 1 ? initLabels(basePanels) : setTimeout(checkForBases, 1000);
-        };
-        const checkForInit = () => {
-            const scriptName = 'CnCTA BaseNavigationBar MR';
-            try {
-                if (typeof qx === 'undefined' || !qx?.core?.Init?.getApplication()?.initDone) return setTimeout(checkForInit, 1000);
-                checkForBases();
-                console.log(`%c${scriptName} loaded`, 'background: #c4e2a0; color: darkred; font-weight:bold; padding: 3px; border-radius: 5px;');
-            } catch (e) {
-                console.error(`%c${scriptName} error`, 'background: black; color: pink; font-weight:bold; padding: 3px; border-radius: 5px;', e);
-            }
-        };
-        checkForInit();
+    const scriptName = 'CnCTA BaseNavigationBar MR';
+    let showGreen = localStorage.getItem(scriptName) !== "false", labels = {}, pop;
+    const rightClickHint = (txt, steps, x, y) => {
+        if (!pop) {
+            pop = new qx.ui.popup.Popup(new qx.ui.layout.Canvas()).set({ padding: 4, backgroundColor: "rgba(0,0,0,0.5)", decorator: "wb-main", opacity: 0.8 });
+            pop.add(pop.__lbl = new qx.ui.basic.Label().set({ textColor: "lime", font: "default", selectable: false }), { left: 0, top: 0 });
+            qx.core.Init.getApplication().getRoot().add(pop);
+        }
+        pop.__lbl.setValue(txt);
+        pop.placeToPoint ? pop.placeToPoint({ left: x, top: y }) : pop.moveTo(x, y);
+        pop.show();
+        qx.event.Timer.once(() => pop.hide(), null, steps);
     };
-    BaseNavigationBarMR();
+    const update = () => {
+        const md = ClientLib.Data.MainData.GetInstance(), step = md.get_Time().GetServerStep(), cities = md.get_Cities().get_AllCities().d;
+        for (const id in labels) {
+            const l = labels[id]; if (!l) continue;
+            const c = cities[id]; if (!c) continue;
+            const end = c.get_MoveCooldownEndStep(), green = step > end, span = webfrontend.phe.cnc.Util.getTimespanString(Math.abs(step - end));
+            (green ? showGreen : true) ? (l.show(), l.set({ value: span, textColor: green ? "darkgreen" : "darkred" })) : l.hide();
+        }
+    };
+    const scan = () => {
+        const app = qx.core.Init.getApplication(), nav = app.getBaseNavigationBar(), panels = nav?.getChildren()?.[0]?.getChildren()?.[0]?.getChildren() || [];
+        if (!nav.__mrInit) {
+            nav.addListener("contextmenu", e => {
+                e.preventDefault();
+                e.stopPropagation();
+                showGreen = !showGreen;
+                localStorage.setItem(scriptName, showGreen);
+                update();
+                rightClickHint(`Green ${showGreen ? "shown" : "hidden"} (time since the last move cooldown ended)`, 3000, e.getDocumentLeft(), e.getDocumentTop());
+            });
+            nav.__mrInit = true;
+        }
+        for (const p of panels) {
+            const id = p.getBaseId?.(); if (!id) continue;
+            if (!labels[id]) labels[id] = new qx.ui.basic.Label();
+            if (!p.getChildren().includes(labels[id])) p.add(labels[id], { bottom: 2, right: 0 });
+        }
+        update();
+        setTimeout(scan, 1000);
+    };
+    (function init() {try {typeof qx === 'undefined' || !qx?.core?.Init?.getApplication()?.initDone ? setTimeout(init, 1000) : (scan(), console.log(`%c${scriptName} loaded`, 'background: #c4e2a0; color: darkred; font-weight:bold; padding: 3px; border: 1px solid black; border-radius: 5px;'))} catch (e) {console.error(`%c${scriptName} error`, 'background: black; color: pink; font-weight:bold; padding: 3px;  border: 1px solid black; border-radius: 5px;', e)}})();
 })();
